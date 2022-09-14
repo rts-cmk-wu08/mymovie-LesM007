b56efe68c02105281d4c313ab082a85f76239afc
LesM007 <77612956+LesM007@users.noreply.github.com>
2022-09-14 08:29:53 +0200

    med genres.js

--- index.js
+++ index.js
@@ -5,25 +5,30 @@ document.addEventListener("DOMContentLoaded", () =>{
     let key = "d27cfb6baa191e1cd0eaa5f32b9e1d80"
 
 
-    fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=d27cfb6baa191e1cd0eaa5f32b9e1d80&language=en-US&page=1")
+    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`)
         .then(response => response.json())
         .then(data => {
 
             console.log(data)
 
+
     data.results.forEach(result => {
         let link = document.createElement("a")
+        let rat = result.vote_average
+        let newRat = Math.round(rat* 10)/ 10
+        link.classList.add("now_img")
         link.setAttribute("href", `details.html?title=${result.title}`)
         link.innerHTML = `
+        <img src="${imgpath+result.poster_path}" alt="movie poster ${result.title}">
         <li>${result.title}</li>
-        <img src="${imgpath+result.poster_path}" alt="movie poster">
+       
             `
         document.body.append(link)
 
             });
         });
 
-    fetch("https://api.themoviedb.org/3/movie/popular?api_key=d27cfb6baa191e1cd0eaa5f32b9e1d80&language=en-US&page=1")
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`)
         .then(response => response.json())
         .then(data => {
             console.log(data)
@@ -32,9 +37,9 @@ document.addEventListener("DOMContentLoaded", () =>{
         let link = document.createElement("a")
         link.setAttribute("href", `details.html?title=${result.title}`)
         link.innerHTML = `
+        <img src="${imgpath+result.poster_path}" alt="movie poster ${result.title}">
         <li>${result.title}</li>
-        <img src="${imgpath+result.poster_path}" alt="movie poster">
-            `
+        `
         document.body.append(link)
     })
         })
